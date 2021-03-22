143063cdddf3cbe145fef085218afffa43995f1a
Saenthuran <saenthuran.vignarajah@ontariotechu.net>
2021-03-22 12:18:22 -0400

    used jquery to get information form input

---
+++ scripts/login.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    console.log("hello");
+    $("#loginButton").click(function(){
+        var password =$("#loginPassword").val();
+        console.log(password);
+    });
+});
+// function saveLoginInformation(){
+//     var temp =$("#password").val();
+// };
