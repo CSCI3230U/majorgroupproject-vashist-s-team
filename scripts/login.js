de67666884ccc3efbdcbe22a285076bfac705e3f
Vethuson <vethuson.amit@ontariotechu.net>
2021-03-22 14:00:30 -0400

    add check to login credentials

--- scripts/login.js
+++ scripts/login.js
@@ -2,8 +2,29 @@
 
 $(document).ready(function(){
     $("#loginButton").click(function(){
+        var validEmail = "False";
+        var validPassword = "False"
         var password =$("#loginPassword").val();
         var email = $("#loginEmail").val();
+        var checkpassword;
+
+        if (email.includes("@") && ((email.includes(".com")) || (email.includes(".net")) || (email.includes(".ca")) )){
+            validEmail = "True"
+            $("#loginEmail").attr('class', 'input'); 
+        }else{
+            validEmail = "False";
+            $("#loginEmail").attr('class', 'input is-danger'); 
+        }
+
+
+        if(password == checkpassword){
+            validPassword = "True";
+            $("#loginPassword").attr('class', 'input'); 
+        }else{
+            validPassword = "False";
+            $("#loginPassword").attr('class', 'input is-danger'); 
+        }
+
         console.log(password);
         console.log(email);
 
