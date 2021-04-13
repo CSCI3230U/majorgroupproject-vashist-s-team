ccff6a7a8cf94598a76ed670679e3f627ffc6d1c
Saenthuran <saenthuran.vignarajah@ontariotechu.net>
2021-04-13 07:45:35 -0400

    fixed some things

---
+++ src/views/logout.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="login">
+        <body id="loginBody">
+            <h1>This is the logout page</h1>
+        </body>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+axios.defaults.withCredentials = true;
+
+export default{
+    // name: App
+
+   computed:{
+        userToken(){
+            return this.$store.state.token;
+        }
+
+    },
+
+
+
+}
+</script>
+
+<style>
+ @import '../assets/styles/HandF.css';
+
+</style>
