4d316bbaf4fa4fb2c0440b60d83d62c4b05df287
Zile Humma <64050632+hummazile@users.noreply.github.com>
2021-04-15 14:14:56 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,51 @@
 
+<template>
+  <body>
+    <div id="app">
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
+      <HandF />
+      <div v-if="userToken">
+            <functionLayout/>
+            <builderPage/>
+      </div>
+      <router-view/>    
+
+    </div>
+  </body>
+  <body>
+    
+    <div class="section is-large my-6">
+    </div>
+    
+  </body>
+</template>
+
+
+<script>
+
+import HandF from './components/HandF.vue'
+import functionLayout from './views/layouts.vue'
+import builderPage from './views/builderPage.vue'
+
+export default {
+    computed:{
+      userToken(){
+          return this.$store.state.token;
+      }
+  },
+  components:{HandF, functionLayout,builderPage},
+  name: 'App'
+}
+</script>
+
+<style>
+body{
+
+    margin:0!important;
+    height:100%!important;
+    overflow: hidden!important;
+    background-color:blanchedalmond!important;
+
+}
+</style>
