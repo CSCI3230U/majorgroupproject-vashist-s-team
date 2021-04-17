24eeb5c7a0bef040d4defeda38792d2291d26aa8
Vashist Patel <vashist.patel@ontariotechu.net>
2021-04-17 19:33:06 -0400

    merged fucntions and import/download/display code

--- src/views/builderPage.vue
+++ src/views/builderPage.vue
@@ -38,6 +38,7 @@ export default({
 }
 
 #import_box{
+    position: relative;
     padding:0;
 }
 
--- src/views/layouts.vue
+++ src/views/layouts.vue
@@ -5,85 +5,88 @@
                                
         </div>
         <div id="navbarLinks" class="navbar-menu">
+
             <div class="navbar-start">
                 <div class="navbar-item has-dropdown is-hoverable ">
                     <a class="navbar-link">
-                        Link 1
+                        Add New...
                     </a>
-                    <div class="navbar-dropdown">
-                        <a class="navbar-item">
-                            Sublink 1
+                    <div id="new" class="navbar-dropdown">
+                        <a id="h1" value="header" class="navbar-item">
+                            Header
                         </a>
                         <hr class="navbar-divider">
-                         <a class="navbar-item">
-                            Sublink 2
+                         <a id="p" value="paragraph" class="navbar-item">
+                            Paragraph
                         </a>
                         <hr class="navbar-divider">
-                         <a class="navbar-item">
-                            Sublink 3
+                         <a id="a" value="link" class="navbar-item">
+                            Link
                         </a>                                              
                     </div>
                 </div>
-                    <div class="navbar-item has-dropdown is-hoverable">
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a id="new-image-link" class="navbar-link">
+                        New Image...
+                    </a>
+                    <div id="image" class="navbar-dropdown">
+                        <a id="small-image" class="navbar-item">
+                            Small
+                        </a>
+                        <hr class="navbar-divider">
+                        <a id="medium-image" class="navbar-item">
+                            Medium
+                        </a>
+                        <hr class="navbar-divider">
+                        <a id="large-image" class="navbar-item">
+                            Large
+                        </a>                                              
+                    </div>
+                </div>
+                <div class="navbar-item has-dropdown is-hoverable">
                     <a class="navbar-link">
-                        Link 2
+                        Text Align...
                     </a>
-                    <div class="navbar-dropdown">
-                        <a class="navbar-item">
-                            Sublink 21
+                    <div id="align" class="navbar-dropdown">
+                        <a id="align-left" class="navbar-item">
+                            Left
                         </a>
                         <hr class="navbar-divider">
-                         <a class="navbar-item">
-                            Sublink 22
+                        <a id="align-centre" class="navbar-item">
+                            Centre
                         </a>
                         <hr class="navbar-divider">
-                         <a class="navbar-item">
-                            Sublink 23
+                        <a id="align-right" class="navbar-item">
+                            Right
                         </a>                                              
                     </div>
                 </div>
-                                <div class="navbar-item has-dropdown is-hoverable">
+                <div class="navbar-item has-dropdown is-hoverable">
                     <a class="navbar-link">
-                        Link 3
+                        Font...
                     </a>
-                    <div class="navbar-dropdown">
-                        <a class="navbar-item">
-                            Sublink 31
+                    <div id="apply-font" class="navbar-dropdown">
+                        <a id="bold-font" class="navbar-item">
+                            Bold
                         </a>
                         <hr class="navbar-divider">
-                         <a class="navbar-item">
-                            Sublink 32
+                        <a id="italic-font" class="navbar-item">
+                            Italic
                         </a>
                         <hr class="navbar-divider">
-                         <a class="navbar-item">
-                            Sublink 33
+                        <a id="underline-font" class="navbar-item">
+                            Underline
                         </a>                                              
                     </div>
                 </div>
-                    <div class="navbar-item has-dropdown is-hoverable">
-                    <a class="navbar-link">
-                        Link 4
-                    </a>
-                    <div class="navbar-dropdown">
-                        <a class="navbar-item">
-                            Sublink 41
-                        </a>
-                        <hr class="navbar-divider">
-                         <a class="navbar-item">
-                            Sublink 42
-                        </a>
-                        <hr class="navbar-divider">
-                         <a class="navbar-item">
-                            Sublink 43
-                        </a>                                              
-                    </div>
-                </div>
-                
+
                 <div class="buttons">
                     <router-link to="/display" class="button is-light" >Display Code</router-link>
                 </div>
-  
+
             </div>
+
+            
             <!-- uplaod button -->
              <div class="navbar-end">
                  <button class="button" v-on:click="updateSave()">Save</button>
@@ -133,6 +136,153 @@
 
 </template>
 <script>
+    // Importing jQuery
+    import jQuery from "jquery";
+    const $ = jQuery;
+    window.$ = $;
+    // A count variable to keep track of all the elements added to the builder page
+    var count = 0;
+    // A variable to keep track of the currently selected element
+    var currentElement;
+    // A function that will append a new element to the building space, depending on
+    // the currently selected element and the type of element to be added
+    function addElement(tag, name, newID, currentElement) {
+        // Creating the new element based on the tag that was passed to the function
+        let element = document.createElement(tag);
+        element.innerHTML = `[New ${name} element created]`;
+        element.id = `${name}${newID}`;
+        // If there is an element currently selected, append the new element after
+        // the currently selected element. Otherwise, append the element to the
+        // bottom of the page
+        if (currentElement) {
+            $(element).insertAfter(`#${currentElement}`);
+        }
+        else {
+            document.querySelector('#import_box').appendChild(element);
+        }
+    }
+    // A function that will append a new image to the building space, depending on
+    // the currently selected element
+    function addImage(size, url, newID, currentElement) {
+        // Creating the new image based on the size that was passed to the function
+        let image = document.createElement('img');
+        image.src = url;
+        image.id = `image${newID}`;
+        image.className = `image ${size}`;
+        // If there is an element currently selected, append the image after the
+        // currently selected element. Otherwise, append the image to the bottom
+        // of the page
+        if (currentElement) {
+            $(image).insertAfter(`#${currentElement}`);
+        }
+        else {
+            document.querySelector('#import_box').appendChild(image);
+        }
+    }
+    window.onload = function() {
+        // Adding the new element that was clicked from the bulma navbar menu. If there
+        // is a current element selected, append the new element underneath the current
+        // element
+        $('#new a').click(function() {
+            count++;
+            addElement($(this).attr('id'), $(this).attr('value'), count, currentElement);
+        });
+        // When an element is clicked, set it to the current element
+        $(document).on('click', '#import_box *', function() {
+            $('#import_box *').removeClass('current');
+            currentElement = $(this).attr('id');
+            $(this).addClass('current');
+        });
+        // If the delete key (del) is pressed, delete the current element and
+        // set the current element to undefined
+        $(document).on('keyup', function(key) {
+            let keyPressed = key.which;
+            if (keyPressed == 46) {
+                $(`#${currentElement}`).remove();
+                currentElement = '';
+            }
+        });
+        // Align the selected element's text when one of the options under align is
+        // selected in the builder navbar
+        $('#align a').click(function() {
+            $(`#${currentElement}`).removeClass('align-left align-centre align-right');
+            $(`#${currentElement}`).addClass($(this).attr('id'));
+        });
+        // Apply the specified font to the selected element
+        $('#apply-font a').click(function() {
+            if ($(`#${currentElement}`).hasClass($(this).attr('id'))) {
+                $(`#${currentElement}`).removeClass($(this).attr('id'));
+            }
+            else {
+                $(`#${currentElement}`).addClass($(this).attr('id'));
+            }
+        });
+        // When an element in the builder is double-clicked, add a text field where the
+        // user can edit the inner-html of that element
+        $(document).on('dblclick', '#import_box *', function() {
+            // Checking to see if we already have a text area to prevent duplicate
+            // textareas from appearing
+            if ($(this).attr('id') != 'tempArea' && $(this).hasClass('image') == false) {
+                // Hidding the current element
+                let currentElement = $(this);
+                currentElement.addClass('hidden');
+                // Adding a temporary textarea for editing
+                let tempTextArea = document.createElement('textarea');
+                tempTextArea.innerHTML = $(this).html();
+                tempTextArea.id = 'tempArea';
+                tempTextArea.className = "textarea has-fixed-size";
+                // Insert the temporary textarea for editing
+                $(tempTextArea).insertAfter($(this));
+                // If enter is pressed, display the current element with the value from
+                // the textarea, and remove the temporary textarea used for editing
+                $(tempTextArea).keypress(function(key) {
+                    let keyPressed = key.which;
+                    if (keyPressed == 13) {
+                        currentElement.html($(this).val());
+                        $(this).remove();
+                        currentElement.removeClass('hidden');
+                    } 
+                });
+            } 
+        });
+        // Adding a new image from the bulma navbar menu based on the selected size
+        $('#image a').click(function() {
+            // Checking to see if the element has already been clicked. If the element
+            // has been clicked, wait until the new photo is added to the builder page
+            if ($(this).hasClass('clicked') == false) {
+                // Adding a class to show that the element has been clicked
+                $(this).addClass('clicked');
+                // A variable to keep track of the original html in the bulma navbar menu
+                let currentHTML =  $(this).html();
+                // Expanding the bulma navbar "New Image..." link so the user can see the
+                // image url that they are entering
+                $('#new-image-link').addClass('expand-link');
+                // Adding a new input field inside the navbar item for the user to enter
+                // their image url
+                $(this).html('<input id="tempImgInput" class="input is-small" type="text" placeholder="Enter Image URL">');
+                // When enter is pressed, create the new image based on the selected size
+                $(this).keypress(function(key) {
+                    let keyPressed = key.which;
+                    if ((keyPressed == 13) && (typeof $(this).children().val() !== 'undefined')) {
+                        console.log($(this).children().val());
+                        let url = $(this).children().val();
+                        count++;
+                        addImage(currentHTML, url, count, currentElement);
+                        $('#new-image-link').removeClass('expand-link');
+                        $(this).html(currentHTML);
+                        $(this).removeClass('clicked');
+                    }
+                    else if (keyPressed == 13) {
+                        $('#new-image-link').removeClass('expand-link');
+                        $(this).html(currentHTML);
+                        $(this).removeClass('clicked');
+                    }
+                });
+                // Focusing on the input field
+                $('#tempImgInput').focus();
+            }
+        });
+    }
     
   export default({
       
@@ -200,6 +350,71 @@
    
 </script>
 <style lang ="css">
+
+
+#import_box h1 {
+    font-size: 2em;
+    font-weight: bold;
+}
+#import_box *:hover {
+    border: 1px solid black;
+    border-radius: 5px;
+}
+#import_box * {
+    border: 1px solid transparent;
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem;
+    word-wrap: break-word;
+}
+/* The currently selected element */
+.current {
+    background: rgb(206, 206, 206);
+    border-radius: 5px;
+}
+/* This class hides an element when it is being edited*/
+.hidden {
+    display: none;
+}
+/* These classes are used to align text */
+.align-left {
+    text-align: left;
+}
+.align-centre {
+    text-align: center;
+}
+.align-right {
+    text-align: right;
+}
+/* This class is used to expand the navbar section when the user inputs a link */
+.expand-link {
+    width: 35rem;
+}
+/* These classes are used to size different images */
+.Small {
+    width: 33.33%;
+    height: auto;
+}
+.Medium {
+    width: 66.66%;
+    height: auto;
+}
+.Large {
+    width: 100%;
+    height: auto;
+}
+/* These classes are used to change the font of an element */
+.bold-font {
+    font-weight: bold;
+}
+.italic-font {
+    font-style: italic;
+}
+.underline-font {
+    text-decoration: underline;
+}
+
+
+
 .modal {
   background-color: rgba(0, 0, 0, 0.4);
 
