f8cff4a2494b8d51caf9221a3065023bef3b79c0
Vethuson <vethuson.amit@ontariotechu.net>
2021-04-19 16:03:16 -0400

    created instructional gif and added them

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -7,36 +7,43 @@
           <button class="question">How to add new text feilds?</button>
           <div class="answer">
             <p>Click on "Add New..." drop down menu and here you can select the type of text feild you want to add.</p>
+            <img class ="instruction" src="../Instructionalgifs/howtoaddtextfield.gif" />
           </div>
 
           <button class="question">How to add new images?</button>
           <div class="answer">
             <p>Click on "New Image..." drop down menu, then select on the size you would like your image to be, paste the url of the image and press enter.</p>
+            <img class ="instruction" src="../Instructionalgifs/howtoaddimage.gif" />
           </div>
 
           <button class="question">How to align text?</button>
           <div class="answer">
             <p>Click on "Alignment" drop down menu and select your desired option.</p>
+            <img class ="instruction" src="../Instructionalgifs/howtoalign.gif" />
           </div>
 
           <button class="question">How to change font?</button>
           <div class="answer">
             <p>First select the text feild of which you want to change the font. Then click on "Font..." drop down mene and select you desired option.</p>
+            <img class ="instruction" src="../Instructionalgifs/howtochangefont.gif" />
           </div>
 
           <button class="question">How to add charts?</button>
           <div class="answer">
             <p>Click on "Charts..." dropdown menu and select the type of chart you want. Then input the chart title and data.</p>
+            <img class ="instruction" src="../Instructionalgifs/howtoaddchart.gif" />
           </div>
 
           <button class="question">How to change the background colour for text feild?</button>
           <div class="answer">
             <p>Click on the text you want of which you want to change the backgroud of, then click "Color Selector" and pick your desired color.</p>
+            <img class ="instruction" src="../Instructionalgifs/howtochangebgtext.gif" />
           </div>
 
           <button class="question">How to change the text colour?</button>
           <div class="answer">
             <p>Click on the text you want to set the color of, then click "Text Colour" and pick your desired color.</p>
+            <img class ="instruction" src="../Instructionalgifs/howtochangefontcolor.gif" />
           </div>
 
 
@@ -44,16 +51,19 @@
           <button class="question">How to import a HTML template?</button>
           <div class="answer">
             <p>Click "File" then "Choose a File" and select your desired file.</p>
+            <img class ="instruction" src="../Instructionalgifs/howtoimport.gif" />
           </div>
 
           <button class="question">How to save your file?</button>
           <div class="answer">
             <p>Click "File" then "Export", give your file a name and click "save".</p>
+            <img class ="instruction" src="../Instructionalgifs/howtosave.gif" />
           </div>
 
           <button class="question">How can I check the code for my website?</button>
           <div class="answer">
             <p>Click on "Display Code" button.</p>
+            <img class ="instruction" src="../Instructionalgifs/howtoshowcode.gif" />
           </div>
 
         </section>
@@ -91,6 +101,11 @@ export default {
 </script>
 
 <style >
+.instruction{
+  height:45rem;
+  width:auto;
+  margin-left:10%;
+}
 .box {
   width: 98%;
 }
@@ -122,6 +137,7 @@ export default {
   overflow: hidden;
   margin-top:10px;
   margin-bottom:10px;
+  text-align: center;
 }
 
 </style>
